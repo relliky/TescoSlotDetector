f4caec680cbc41915a7d377c2fb2793401715451
tai <tailishome@gmail.com>
2020-04-09 00:55:54 +0100

    Added push notification to smart phone.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # TescoSlotDetector
 
 # Info
-Detects if slots are available on Tesco's website. Allows a user to login and add items to a basket. On checkout the script will iterate over each day for three weeks forever. If an available slot is found, the program will activate a beep.
+Detects if slots are available on Tesco's website. Allows a user to login and add items to a basket. On checkout the script will iterate over each day for three weeks forever. If an available slot is found, the program will send a notification to your phone. 
+
 
 # Notes
 Requires chromedriver to function, download from https://chromedriver.chromium.org.
+It is using Pushover.net for pushing smart phone notification. Set up account, API, user-key and replace my api and userkey in the script.
--- run.py
+++ run.py
@@ -1,19 +1,29 @@
 # Tesco Booker v1.0
-# Download chromedriver for script to function
+# Download chromedriver for script to function 
 
-from selenium import webdriver
 import time
 import sys
 import os
 from datetime import datetime
 from datetime import timedelta
+from selenium import webdriver  # need to be installed
+from pushover import Client     # need to be installed
+
+# Send notification to phone using pushover
+# This is my personal TOKEN and API, please do not use them for testing
+client = Client("uj4ktz2oah8huuc9kk7g6awg6gactw", api_token="abe4opagsun1xdiya5c5fxykyvj2o4") # user-key and token 
+
+start_date = (datetime.now() + timedelta(days=13) ).strftime('%m-%d')
+end_date   = (datetime.now() + timedelta(days=21) ).strftime('%m-%d')
+
+client.send_message("Searching slots from " + start_date + " to " + end_date, title="Start to find available Tesco slots!")
 
 # Load driver
 if os.name == 'nt':
   import winsound
-  driver = webdriver.Chrome('./driver/chromedriver.exe')
+  driver = webdriver.Chrome(r'C:\Users\rellik\Desktop\Workspace\TescoSlotDetector-master\chromedriver_win32\chromedriver.exe')
 else:
-  driver = webdriver.Chrome('./driver/chromedriver')
+  driver = webdriver.Chrome('/Users/Like/Documents/tesco/chromedriver')
 
 driver.set_window_position(0, 0)
 driver.set_window_size(800, 900)
@@ -28,18 +38,23 @@ while current_url != "https://www.tesco.com/groceries/en-GB/slots/delivery":
     time.sleep(2)
 
 # Loop forever
-out = 0
+loop_cnt = 1
 while True:
 
+  print ("\nStarting " + str(loop_cnt) + "th Tesco slot searches.")
+  out = 0
+
   # Iterate over days for three weeks
-  for i in range(21):
+  for i in range(13, 21):
     date = (datetime.now() + timedelta(days=i) ).strftime('%Y-%m-%d')
     url = "https://www.tesco.com/groceries/en-GB/slots/delivery/" + date + "?slotGroup=1"
     driver.get(url);
 
-    print ("Checking " + str(date))
+    time.sleep(1)
+    print ("Checking Tesco Slot " + str(date))
 
     # Check for slot message and break if didnt find element
+    # TODO: Seems sometimes it gets stuck and make no progress further. Investigate this problem.
     try:
       slot_message = driver.find_element_by_class_name('slot-list--none-available')
     except:
@@ -47,18 +62,16 @@ while True:
       break
 
     # Sleep
-    time.sleep(1.5)
+    time.sleep(0.5)
 
+
+  loop_cnt = loop_cnt + 1
+  
+  if ((loop_cnt % 1000) == 0) and out == 0:
+    client.send_message("Completed " + str(loop_cnt) + " searches.", title="Milestone")
+    
   # Break out main loop
   if out == 1:
-    break
+    client.send_message("Slots in " + date + ", resume searching in 1 min.", title="Current in Queue OR available Tesco Slot Found!")
+    time.sleep(60)
 
-# Cause sound for detection of a available slot
-for i in range(15):
-  if os.name == 'nt':
-    frequency = 2500
-    duration = 1000
-    winsound.Beep(frequency, duration)
-  else:
-    sys.stdout.write('\a')
-    sys.stdout.flush()
