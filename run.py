20d2e31bc9dc624e7388cfad12a348ab31703571
tai <tailishome@gmail.com>
2020-04-09 12:26:15 +0100

    Split notification conditions of hitting a queue or hitting a free slot.

--- run.py
+++ run.py
@@ -13,21 +13,30 @@ from pushover import Client     # need to be installed
 # This is my personal TOKEN and API, please do not use them for testing
 client = Client("uj4ktz2oah8huuc9kk7g6awg6gactw", api_token="abe4opagsun1xdiya5c5fxykyvj2o4") # user-key and token 
 
-start_date = (datetime.now() + timedelta(days=13) ).strftime('%m-%d')
-end_date   = (datetime.now() + timedelta(days=21) ).strftime('%m-%d')
-
-client.send_message("Searching slots from " + start_date + " to " + end_date, title="Start to find available Tesco slots!")
-
 # Load driver
 if os.name == 'nt':
   import winsound
-  driver = webdriver.Chrome(r'C:\Users\rellik\Desktop\Workspace\TescoSlotDetector-master\chromedriver_win32\chromedriver.exe')
+  driver = webdriver.Chrome(r'C:\Users\rellik\Documents\GitHub\TescoSlotDetector\chromedriver_win32\chromedriver.exe')
 else:
   driver = webdriver.Chrome('/Users/Like/Documents/tesco/chromedriver')
 
 driver.set_window_position(0, 0)
 driver.set_window_size(800, 900)
 
+#queue_found = 0
+#while True:
+#  driver.get(r"C:\Users\rellik\Desktop\Tesco Groceries.html")
+#    
+#  try:
+#    slot_message = driver.find_elements_by_xpath("//*[contains(text(), 'in a queue to shop with us')]")
+#    queue_found = 1
+#  except:
+#    queue_found = 0
+#
+#  if queue_found == 1:
+#    print ("queue found")
+#  time.sleep(100000)
+  
 # Load url
 driver.get("https://secure.tesco.com/account/en-GB/login")
 current_url = driver.current_url
@@ -45,7 +54,7 @@ while True:
   out = 0
 
   # Iterate over days for three weeks
-  for i in range(13, 21):
+  for i in range(21):
     date = (datetime.now() + timedelta(days=i) ).strftime('%Y-%m-%d')
     url = "https://www.tesco.com/groceries/en-GB/slots/delivery/" + date + "?slotGroup=1"
     driver.get(url);
@@ -53,8 +62,8 @@ while True:
     time.sleep(1)
     print ("Checking Tesco Slot " + str(date))
 
-    # Check for slot message and break if didnt find element
-    # TODO: Seems sometimes it gets stuck and make no progress further. Investigate this problem.
+    # Check for slot message and break the for loop if didnt find element
+    # TODO: Seems sometimes it gets stuck and make no progress further unless hit enter in terminal. Investigate this problem. 
     try:
       slot_message = driver.find_element_by_class_name('slot-list--none-available')
     except:
@@ -64,14 +73,20 @@ while True:
     # Sleep
     time.sleep(0.5)
 
-
-  loop_cnt = loop_cnt + 1
-  
-  if ((loop_cnt % 1000) == 0) and out == 0:
-    client.send_message("Completed " + str(loop_cnt) + " searches.", title="Milestone")
-    
-  # Break out main loop
+  # Found slots or get into a queue
   if out == 1:
-    client.send_message("Slots in " + date + ", resume searching in 1 min.", title="Current in Queue OR available Tesco Slot Found!")
-    time.sleep(60)
+    # Test if it is a queue page
+    not_in_queue = 0
+    try:
+      slot_message = driver.find_elements_by_xpath("//*[contains(text(), 'in a queue to shop with us')]")
+    except:
+      not_in_queue = 1
+    
+    if not_in_queue == 0:
+      client.send_message("Please mannualy refresh the queue page.", title="We are in a Tesco online queue!")
+      time.sleep(1800)
+    else:
+      client.send_message("Check " + date + " slots in broswer, will resume searching in 10 min.", title="Available Tesco Slot Found!")
+      print("Slots found in " + date + ".")
+      time.sleep(600)
 
